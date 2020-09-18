58913619279bc54a4ed578b372d87b231a1a8282
koheian <koheian11@gmail.com>
2020-09-18 16:47:12 +0900

    change the location of quxpy/

--- python/quxpy/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from .quxpy import metro
-    from .quxpy import osc_receiver
-    from .quxpy import osc_sender
-    from .quxpy import process_event
-    from .quxpy import thread_event
-except ModuleNotFoundError:
-    print(" Some modules does not work because the dependencies are not installed collectly.")
--- python/quxpy/example_list_util.py
+++
@@ -1,5 +0,0 @@
-from quxpy import list_util
-
-test_list = [1, [2,3] , [ [(4,5),6] ,7,8] ]
-
-print(list_util.flatten(test_list))
