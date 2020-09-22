2cbe23cbdbe75dd66a20d5b4a7e2d201d5eedb42
koheian <koheian11@gmail.com>
2020-09-22 18:57:27 +0900

    change the type of osc messages

--- python/get_array.py
+++ python/get_array.py
@@ -6,7 +6,9 @@ def get_row_array(img, raw_number):
     row_array = img[raw_number]
     return row_array
 
-def get_beat_list(long_array, list_size):
-    return long_array[0:list_size].tolist()
-    
-    
+def get_beat_list(long_array, i_start, len_list):
+    return long_array[i_start:(i_start+len_list)].tolist()
+ 
+def get_beat_int(long_array, index):
+    a = long_array[index].tolist()
+    return a
--- python/main.py
+++ python/main.py
@@ -1,20 +1,34 @@
 from PIL import Image
 import numpy as np
 from quxpy import osc_sender
+from quxpy import metro
 import config
 import get_array
+import time
+interval = 0.5
+i = 0
 
+# open an edge image
+image_filenames = ['laplacian.jpg', 'canny.jpg', 'sobel.jpg']
+img = np.array(Image.open(image_filenames[0]))
 # configure remote address and port 
-osc_sender.setup("127.0.0.1", 50000)
+osc_sender.setup("127.0.0.1", 50001)
 # test
 osc_sender.send("/foo", 0.1, 0.2)
-# open an edge image
-img = np.array(Image.open('laplacian.jpg'))
-#img = np.array(Image.open('canny.jpg'))
-#img = np.array(Image.open('sobel.jpg'))
 
-# get buffer
-beat_list = get_array.get_beat_list(get_array.get_row_array(img, 500), config.array_size)
+# setup metro
+def send_int():
+    global i
+    # get buffer
+    beat_int = get_array.get_beat_int(get_array.get_row_array(img, 500), i)
+    i = (i + 1) % config.l_img
+    osc_sender.send("/note", beat_int)
 
+metro.add(send_int, interval)
 # send messege
-osc_sender.send("/list", beat_list)
+try:
+    metro.start(use_process=False) 
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:    
+    metro.terminate()
